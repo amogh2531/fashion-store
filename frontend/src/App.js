55da2bdb0946ebe161e1a46f774f1d9a50d9a256
Rahul Sharma <54216803+rahul4200@users.noreply.github.com>
2023-02-20 12:19:56 +0530

    Step 6 completed. Design and build the ProductScreen component done.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 function App() {
 	return (
@@ -18,6 +19,7 @@ function App() {
 				bgColor='gray.200'>
 				<Routes>
 					<Route path='/' element={<HomeScreen />} />
+					<Route path='/product/:id' element={<ProductScreen />} />
 				</Routes>
 			</Flex>
 			<Footer />
