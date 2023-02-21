18c36f3f490c747c0101b0b4f4a1db2de3a25919
Rahul Sharma <54216803+rahul4200@users.noreply.github.com>
2023-02-21 11:35:20 +0530

    Step 13 completed. Connect to the database done.

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 import dotenv from 'dotenv';
 import express from 'express';
 
+import connectDB from './config/db.js';
 import products from './data/products.js';
 
 dotenv.config();
+
+connectDB();
 
 const app = express();
 
