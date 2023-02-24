328f2f4c91753a061a9ecbc91b8b35bea67c9240
Rahul Sharma <54216803+rahul4200@users.noreply.github.com>
2023-02-24 11:14:14 +0530

    Step 28 completed. Cart Functionality done.

---
+++ frontend/src/reducers/cartReducer.js
@@ -0,0 +1,23 @@
+import { CART_ADD_ITEM, CART_REMOVE_ITEM } from '../constants/cartConstants';
+
+export const cartReducer = (state = { cartItems: [] }, action) => {
+	switch (action.type) {
+		case CART_ADD_ITEM:
+			const item = action.payload;
+			const existsItem = state.cartItems.find(
+				(i) => i.product === item.product
+			);
+			if (existsItem) {
+				return {
+					...state,
+					cartItems: state.cartItems.map((i) =>
+						i.product === existsItem.product ? item : i
+					),
+				};
+			} else {
+				return { ...state, cartItems: [...state.cartItems, item] };
+			}
+		default:
+			return state;
+	}
+};
