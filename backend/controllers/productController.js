499f846853267678232fb8816722a9ce374a95a0
Rahul Sharma <54216803+rahul4200@users.noreply.github.com>
2023-02-27 11:33:11 +0530

    Step 33 completed. Creating custom Authentication Middleware to access protected routes done.

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -2,11 +2,21 @@ import asyncHandler from 'express-async-handler';
 
 import Product from '../models/productModel.js';
 
+/**
+ * @desc 		Get all products
+ * @route		GET /api/products
+ * @access	public
+ */
 const getProducts = asyncHandler(async (req, res) => {
 	const products = await Product.find({});
 	res.json(products);
 });
 
+/**
+ * @desc 		Get single product
+ * @route		GET /api/products/:id
+ * @access	public
+ */
 const getProductById = asyncHandler(async (req, res) => {
 	try {
 		const product = await Product.findById(req.params.id);
--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,6 +3,11 @@ import asyncHandler from 'express-async-handler';
 import User from '../models/userModel.js';
 import generateToken from '../utils/generateToken.js';
 
+/**
+ * @desc 		Auth User
+ * @route		POST /api/users/login
+ * @access	public
+ */
 const authUser = asyncHandler(async (req, res) => {
 	const { email, password } = req.body;
 
@@ -22,4 +27,25 @@ const authUser = asyncHandler(async (req, res) => {
 	}
 });
 
-export { authUser };
+/**
+ * @desc 		Get user profile
+ * @route		GET /api/users/profile
+ * @access	private
+ */
+const getUserProfile = asyncHandler(async (req, res) => {
+	const user = await User.findById(req.user._id);
+
+	if (user) {
+		res.json({
+			_id: user._id,
+			name: user.name,
+			email: user.email,
+			isAdmin: user.isAdmin,
+		});
+	} else {
+		res.status(404); // Not found
+		throw new Error('User not found');
+	}
+});
+
+export { authUser, getUserProfile };
