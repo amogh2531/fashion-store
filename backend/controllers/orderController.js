4203c12243673db5caac7ab2cb13d89b59f294e6
Rahul Sharma <54216803+rahul4200@users.noreply.github.com>
2023-03-02 11:00:46 +0530

    Step 49 completed. Get Order By ID (Backend Endpoint) done.

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -38,4 +38,22 @@ const addOrderItems = asyncHandler(async (req, res) => {
 	}
 });
 
-export { addOrderItems };
+/**
+ * @desc 		Get order by ID
+ * @route		GET /api/orders/:id
+ * @access	private
+ */
+const getOrderById = asyncHandler(async (req, res) => {
+	const order = await Order.findById(req.params.id).populate(
+		'user',
+		'name email'
+	);
+
+	if (order) {
+		res.json(order);
+	} else {
+		throw new Error('Order not found');
+	}
+});
+
+export { addOrderItems, getOrderById };
