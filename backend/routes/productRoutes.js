6fa8ca2d8d5865515d4987ee8602def4083bcc81
Rahul Sharma <54216803+rahul4200@users.noreply.github.com>
2023-03-06 11:47:02 +0530

    Step 66 completed. Create and Update Product Backend Endpoints done.

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,17 +1,23 @@
 import express from 'express';
 
 import {
+	createProduct,
 	deleteProduct,
 	getProductById,
 	getProducts,
+	updateProduct,
 } from '../controllers/productController.js';
 
 import { admin, protect } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
-router.route('/').get(getProducts);
+router.route('/').get(getProducts).post(protect, admin, createProduct);
 
-router.route('/:id').get(getProductById).delete(protect, admin, deleteProduct);
+router
+	.route('/:id')
+	.get(getProductById)
+	.delete(protect, admin, deleteProduct)
+	.put(protect, admin, updateProduct);
 
 export default router;
